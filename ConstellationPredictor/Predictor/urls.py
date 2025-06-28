96971895b2aeaffe9214b73dff7176fea0e99135
unknown <179501622+Rstar-910@users.noreply.github.com>
2025-06-28 23:38:49 +0530

    first commit

---
+++ ConstellationPredictor/Predictor/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('detect/', views.detect_view, name='detect'),
+path('detect/process/', views.process_frame, name='process_frame'),
+path('chatbot/', include('chatbot.urls')),
+path('locator/', include('Locator.urls'),name='Locator'),
+path('database/', views.database, name='database'),
+    path('predict/', views.predict, name='predict'),
+    path('upload/',views.upload,name='upload'),
+    path('process-upload/', views.process_upload, name='process_upload'),
+    
+]
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("ROOT OF THE PROJECT CREATED BY UV")
+
+
+if __name__ == "__main__":
+    main()
