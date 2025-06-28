c5e6831f0c6e48e134fc779298f33597ae76088c
Rakshit Verma <146798265+rakshverma@users.noreply.github.com>
2025-06-29 03:16:44 +0530

    Update urls.py

--- ConstellationPredictor/Predictor/urls.py
+++ ConstellationPredictor/Predictor/urls.py
@@ -10,6 +10,7 @@ path('locator/', include('Locator.urls'),name='Locator'),
 path('database/', views.database, name='database'),
     path('predict/', views.predict, name='predict'),
     path('upload/',views.upload,name='upload'),
+    path('get-constellation-info/', views.get_constellation_info, name='get_constellation_info'),
     path('process-upload/', views.process_upload, name='process_upload'),
     
 ]
